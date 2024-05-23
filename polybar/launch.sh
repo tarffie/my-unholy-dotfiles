333c10ce43b7a252d2ac1083665473b886d32f10
yoru <yoru@mothership.mothership.one>
2024-05-23 14:43:50 -0300

    new configs just released

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -1,10 +1,10 @@
-#!/usr/bin/env bash
+#!/usr/bin/env sh
 
 # Terminate already running bar instances
-# If all your bars have ipc enabled, you can use 
-# Otherwise you can use the nuclear option:
 killall -q polybar
 
-# Launch bar1 and bar2
-echo "---" | tee -a /tmp/polybar1.log
-polybar bar 2>&1 | tee -a /tmp/polybar1.log & disown
+# Wait until the processes have been shut down
+while pgrep -u $UID -x polybar >/dev/null; do sleep 0.5; done
+
+# Launch bar1
+polybar -c ~/.config/polybar/config main &
