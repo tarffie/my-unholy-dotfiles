8980c27599c3dbfa1ae5b4ef48552a156cdd8d0d
Emilia <emiliaalves.tarffie@gmail.com>
2025-07-13 16:37:56 -0300

    feat: new script to sync content from host to vm

---
+++ scripts/sync_vm.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+if [ $# -ne 2 ]; then
+  echo "Usage: sync_vm <target_directory> <ip_address>"
+  exit 1 
+fi
+
+TARGET_DIR="$1"
+IP_ADDR="$2"
+
+rsync -avz --delete "$TARGET_DIR" "developer@$IP_ADDR:/home/developer/projects"
