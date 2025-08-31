38eecab4aed7c034e0c85f09d06be4a92d317a29
Emilia <emiliaalves.tarffie@gmail.com>
2025-08-31 13:03:32 -0300

    fix: didn't fully fixed screenshots and screenshots script had an error

--- scripts/take-screenshot.sh
+++ scripts/take-screenshot.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-path=$HOME/personal/screenshots
+path=$HOME/personal/screenshots/
 date="`date "+%Y-%m-%d-%H-%M-%S"`"
 screenshot="$path$date-screenshot.png"
 /usr/local/bin/import $screenshot
